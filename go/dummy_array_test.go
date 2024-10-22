e3dfb717d8cb6acbc4b8125594ce2e78ef8c9fdb
ant <labian911@gmail.com>
2024-10-22 09:10:17 +0200

    added benchmark for DummyArrayNaive and DummyArrayBetter

--- go/dummy_array_test.go
+++ go/dummy_array_test.go
@@ -49,6 +49,37 @@ func TestDummyArrayBetter(t *testing.T) {
 	}
 }
 
-func BenchmarkAdd(b *testing.B) {
+func doStuffToDummyArray(constructor func(uint) DummyArray) {
+	//initialization
+	da := constructor(1000000)
+	var value uint
+	//adding
+	value = 0
+	for i := 0; i < 1000000; i++ {
+		da.Add(value)
+		value = (value + 3) % 1000000
+	}
+	//removing
+	value = 0
+	for i := 0; i < 1000000; i++ {
+		da.Remove(value)
+		value = (value + 3) % 1000000
+	}
+	//checking exists
+	value = 0
+	for i := 0; i < 1000000; i++ {
+		da.Exists(value)
+		value = (value + 3) % 1000000
+	}
+}
 
+func BenchmarkDummyArrayNaive(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		doStuffToDummyArray(func(u uint) DummyArray { return NewDummyArrayNaive(u) })
+	}
+}
+func BenchmarkDummyArrayBetter(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		doStuffToDummyArray(func(u uint) DummyArray { return NewDummyArrayBetter(u) })
+	}
 }
