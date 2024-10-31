c5c66cd0a9a1710f4fb267758a1de330aff8d380
B-a-r-r <clement.barriere@etu.u-bordeaux.fr>
2024-10-31 15:54:48 +0100

    les dummy-arrays en Rust, c'est ok ;)

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,53 +1,7 @@
+mod unit_tests;
 mod dummy_array;
-use std::clone;
 
-use dummy_array::DummyArrayVec;
-
-fn main() {
-    let mut my_dummy_array = DummyArrayVec::new(2).unwrap();
-    print!("{}", my_dummy_array.repr());
-
-    // // clone the array
-    // let clone_dummy_array = my_dummy_array.clone();
-    // assert_eq!(true, my_dummy_array.partial_eq(&clone_dummy_array), "The two arrays should be equal. ");
-
-    // no value should exist in the array at this point
-    assert_ne!(true, my_dummy_array.exists(0), "Value 0 should not exist. ");
-    // the length+1 value should not be recognized as a stored value
-    assert_ne!(true, my_dummy_array.exists(1), "Value 1 should not exist. ");
-
-    // add a value to the empty array
-    assert_eq!(true, my_dummy_array.add(1).unwrap(), "Value 1 should be added. ");
-    // add a value to the array
-    assert_eq!(true, my_dummy_array.add(0).unwrap(), "Value 0 should be added. ");
-
-    print!("{}", my_dummy_array.repr());
-
-    // add twice the same value
-    assert_eq!("Value already exists. ", my_dummy_array.add(1).unwrap_err(), "Value 1 should not be added. ");
-    // add a value that is not valid
-    assert_eq!("Not a valid value. ", my_dummy_array.add(-1).unwrap_err(), "Value -1 should not be added. ");
-    // add a value greater than the length
-    assert_eq!(true, my_dummy_array.add(2).unwrap(), "Value 2 should be added. ");
-
-    print!("{}", my_dummy_array.repr());
-
-    // search for the value that was added
-    assert_eq!(true, my_dummy_array.exists(1), "Value 1 should exist. ");
-
-    // get the value that was added
-    assert_eq!(1, my_dummy_array.get(1).unwrap(), "Value 1 should be retrieved. ");
-    
-    // remove a non-existing value
-    assert_eq!("Value not found. ", my_dummy_array.remove(4).unwrap_err(), "Value 4 should not be removed. ");
-
-    print!("{}", my_dummy_array.repr());
-
-    // remove the value that was added
-    assert_eq!(true, my_dummy_array.remove(1).unwrap(), "Value 1 should be removed. ");
-
-    print!("{}", my_dummy_array.repr());
-
-    // search for the value that was removed
-    assert_eq!(false, my_dummy_array.exists(1), "Value 1 should not exist. ");
+fn main() 
+{
+    unit_tests::main(100);
 }
--- rust/target/debug/deps/rust.d
+++ rust/target/debug/deps/rust.d
@@ -1,6 +1,7 @@
-C:\Users\cleme\Documents\Programmation\Rust\dummy-array\rust\target\debug\deps\rust.exe: src/main.rs src\dummy_array.rs
+C:\Users\cleme\Documents\Programmation\Rust\dummy-array\rust\target\debug\deps\rust.exe: src/main.rs src\unit_tests.rs src\dummy_array.rs
 
-C:\Users\cleme\Documents\Programmation\Rust\dummy-array\rust\target\debug\deps\rust.d: src/main.rs src\dummy_array.rs
+C:\Users\cleme\Documents\Programmation\Rust\dummy-array\rust\target\debug\deps\rust.d: src/main.rs src\unit_tests.rs src\dummy_array.rs
 
 src/main.rs:
+src\unit_tests.rs:
 src\dummy_array.rs:
